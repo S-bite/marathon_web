234d2cd4b28720c8e4ed8f63831893152a44f61d
S-bite <0125bitbite@gmail.com>
2019-12-16 00:27:38 +0900

    wip

--- static/viewer.js
+++ static/viewer.js
@@ -1,21 +1,26 @@
-// Two different data types here: strings and numbers
-// Differentiate between strings and variables
-
-// Key words: function and var
-
-function setup() {
-    var canvas = createCanvas(800, 200)
-    // jumbo-canvas is a string
-    canvas.parent('viewer-canvas')
-    // background(255, 0, 200)
+function handleOutputFiles(file, callback) {
+    var ctx = document.getElementById("canvas").getContext("2d");
+    ctx.strokeStyle = "rgb(0,0,0)";
+    var reader = new FileReader();
+    reader.readAsText(file[0]);
+    reader.onload = function () {
+        var rawOutputList = reader.result.split(/[\s\n]/);
+        var lines = Number(rawOutputList[0]);
+        if (isNaN(lines)) {
+            /*TODO:err@数ではありません*/
+        }
+        if (lines != rawOutputList.length * 4 - 1) {
+            /*TODO:err@数が一致しません*/
+        }
+        for (var i = 1; i < lines; i += 4) {
+            var y1 = Number(rawOutputList[i]);
+            var x1 = Number(rawOutputList[i + 1]);
+            var y2 = Number(rawOutputList[i + 2]);
+            var x2 = Number(rawOutputList[i + 3]);
+            /*TODO:非数のときのハンドリング */
+            ctx.strokeRect(x1, y1, x2 - x1, y2 - y1);
+        }
+        console.log(lines);
+    }
 }
 
-var tronX = 50
-var speed = 5
-
-function draw() {
-    x = 1000 * Math.random()
-    y = 1000 * Math.random()
-
-    ellipse(x, y, x + 2, y + 2)
-}
